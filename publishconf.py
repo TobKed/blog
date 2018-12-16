97c93008a8f7e3d4c60144c009e27fefe9d8ee2f
TobKed <tobiaszkedzierski@gmail.com>
2018-12-16 15:18:52 +0100

    add 13

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
-RELATIVE_URLS = False
+SITEURL = 'blog/'
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
