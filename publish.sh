55ddba3f92086a16ffc5840abc743acee6e0f6e8
TobKed <tobiaszkedzierski@gmail.com>
2018-12-16 15:08:15 +0100

    update x

--- publish.sh
+++ publish.sh
@@ -2,11 +2,9 @@
 echo 'commit message:'
 read commit_message
 echo 'pushing output folder (production version) to master...'
-#pelican content -o output -s publishconf.py
-pelican content -o output -s publishconf.py
+pelican content
 git add ./output
 git add ./content
 git commit -m "$commit_message"
 git push origin master || echo "push failed"
 git subtree push --prefix output origin gh-pages || echo "push failed"
-#pelican content -o output
--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'blog/'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
