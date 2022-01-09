b0851aec998fc822038812fa3d2084f5c2e48c12
Tobiasz Kędzierski <tobiasz.kedzierski@netguru.com>
2022-01-09 17:33:34 +0100

    Refactor photos resizing

--- scripts/resize_photos/main.py
+++ scripts/resize_photos/main.py
@@ -1,11 +1,21 @@
-from PIL import Image
-
 import pathlib
 
-maxsize = (1024, 1024)
-for input_img_path in pathlib.Path("input").iterdir():
-    output_img_path = str(input_img_path).replace("input", "output")
-    with Image.open(input_img_path) as im:
-        im.thumbnail(maxsize)
-        im.save(output_img_path, "JPEG", dpi=(300, 300))
-        print(f"processing file {input_img_path} done...")
+from PIL import Image
+
+
+MAXSIZE = (1024, 1024)
+DPI = (300, 300)
+
+INPUT_PATH = "input"
+OUTPUT_PATH = "output"
+
+
+for input_img_path in pathlib.Path(INPUT_PATH).iterdir():
+    output_img_path = str(input_img_path).replace(INPUT_PATH, OUTPUT_PATH)
+    try:
+        with Image.open(input_img_path) as im:
+            im.thumbnail(MAXSIZE)
+            im.save(output_img_path, dpi=DPI)
+            print(f"processing file {input_img_path} done...")
+    except Exception as e:
+        print(f"\t{e}")
